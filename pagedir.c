66a7d8c5d6153429b48603c34ddf2471aa1f5c4e
dymtran <130620669+dymtran@users.noreply.github.com>
2023-06-07 22:59:58 -0700

    Update pagedir.c

--- pagedir.c
+++ pagedir.c
@@ -1,7 +1,7 @@
 #include "pagedir.h"
 #include <stdio.h>
 
-bool pagedir_init(const char *pageDirectory) {
+/*bool pagedir_init(const char *pageDirectory) {
     // Fill in with your implementation
     // we need to create a file named .crawler in pageDirectory.
     char filePath[150];
@@ -21,3 +21,28 @@ void pagedir_save(const webpage_t *page, const char *pageDirectory, const int do
     // Fill in with your implementation
 	 
 }
+*/
+
+bool pagedir_init(const char *pageDirectory) {
+	if (pageDirectory == NULL) {
+		return false;
+	}
+	
+	char file[100];
+	sprintf(file, sizeof(file), "%s/crawler", pageDirectory);
+	
+	FILE *open = fopen(file, "r+"):
+	
+	if (open == NULL) {
+		return false;
+	}
+	
+	fclose(open);
+	return true;
+}
+
+void pagedir_save(const webpage_t *page, const char *pageDirectory, const int documentID) {
+    // Fill in with your implementation
+	 
+}
+
