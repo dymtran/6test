6685acf433263d22b5f69511ccfe1e73b3f9312f
Dylan Tran <dymtran@ucsc.edu>
2023-06-07 22:56:49 -0700

    eysadsa

--- pagedir.c
+++ pagedir.c
@@ -1,19 +1,23 @@
 #include "pagedir.h"
+#include <stdio.h>
 
 bool pagedir_init(const char *pageDirectory) {
     // Fill in with your implementation
     // we need to create a file named .crawler in pageDirectory.
     char filePath[150];
     snprintf(filePath, sizeof(filePath), "%s/.crawler", pageDirectory);
+    printf("%s", filePath);
     FILE *file = fopen(filePath, "r+");
     if (file == NULL) {
         printf("Error opening file\n");
         return false;
-    
+    }
+
     fclose(file);
     return true;
 }
 
 void pagedir_save(const webpage_t *page, const char *pageDirectory, const int documentID) {
     // Fill in with your implementation
+	 
 }
