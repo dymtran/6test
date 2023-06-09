7af9fccf0c417e8f36a45bd4c5aa018950ed73ec
Dylan Tran <dymtran@ucsc.edu>
2023-06-08 19:55:33 -0700

    sdadqwdqqwdqdqq

--- crawler.h
+++ crawler.h
@@ -19,6 +19,15 @@ typedef struct {
 
 typedef struct {
 	// Fill in with your implementation
+	//int num_slots;
+	//set_t **sets;
 } hashtable_t;
+
+webpage_t *webpage_new(char *url, int depth, char *html);
+
+bag_t *bag_new(int capacity);
+
+void bag_insert(bag_t *bag, webpage_t *page);
+
 
 #endif
